cd5f9fea8914571295a2647b9937fc74717bb2d7
Thiago Demas <thiago.demas@flinsider.com.br>
2023-11-19 12:46:07 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server running!!')
+  })
